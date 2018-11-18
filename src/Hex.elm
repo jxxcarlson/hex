aa5e1a54c4682a90c17d68bca8b66fb8cdcac58b
James Carlson <jxxcarlson@mac.com>
2018-11-18 09:34:13 -0500

    Add int to hex converters

--- src/Hex.elm
+++ src/Hex.elm
@@ -1,45 +1,27 @@
-module Hex exposing(hex, encodeString, decodeString)
+module Hex exposing(decode, hexStringOfInt, intOfHexDigit, intOfHexPair)
 
 {-|  The function call `hex someBytes` gives 
 a hexadecimal view of the contents of 
 `someBytes : Bytes`.
 
-@docs hex, encodeString, decodeString
+@docs decode
 -}
 
 import Bytes exposing (Bytes, Endianness(..))
-import Bytes.Decode as Decode exposing (..)
+import Bytes.Decode as Decode exposing (Decoder, Step(..), loop,  map, succeed)
 import Bytes.Encode as Encode exposing(encode)
 
 
-{-| > encodeString "Hello" |> hex
-Just "48656C6C6F" : Maybe String
+{-| Encode.encode (Encode.string "Hello") |> Hex.decode == "48656C6C6F" 
 -}
-hex : Bytes -> Maybe  String 
-hex bytes_ = 
+decode : Bytes -> String 
+decode bytes_ = 
   bytes_  
-    |> decode (decodeBytes (Bytes.width bytes_) Decode.unsignedInt8)
+    |> Decode.decode (decodeBytes (Bytes.width bytes_) Decode.unsignedInt8)
     |> Maybe.map (List.reverse >> (List.map hexStringOfInt) >> String.join "")
+    |> Maybe.withDefault "Error"
 
 
-
-{-| > encodeString "$20"
-<3 bytes> : Bytes.Bytes
--}
-encodeString : String -> Bytes 
-encodeString  str = 
-  encode (Encode.string str)
-
-
-{-| > encodeString "$20$" |> decodeString
-Just "$20$" : Maybe String
--}  
-decodeString : Bytes -> Maybe String 
-decodeString bytes = 
-  let 
-    n = Bytes.width bytes
-  in
-  decode (Decode.string n) bytes
 
 
 stringOfHexDigit : Int -> String  
@@ -56,6 +38,8 @@ stringOfHexDigit n =
     15 -> "F"
     _ -> "X" 
 
+{-|  hexStringOfInt 247 == "F7"
+-}
 hexStringOfInt : Int -> String
 hexStringOfInt b =
   let 
@@ -64,15 +48,33 @@ hexStringOfInt b =
  in 
    (stringOfHexDigit hi) ++ (stringOfHexDigit low)
   
+intOfHexDigit : String -> Maybe Int
+intOfHexDigit str = 
+  if List.member str ["0", "1", "2", "3", "4", "5", "6", "7", "9", "9"] then 
+    String.toInt str  
+  else 
+    case str of 
+       "A" -> Just 10
+       "B" -> Just 11
+       "C" -> Just 12
+       "D" -> Just 13
+       "E" -> Just 14
+       "F" -> Just 15
+       _ -> Nothing
 
-{-| 
-> abc = encodeString "abc"
-<3 bytes> : Bytes
-> abc 
-       |> decode (decodeNBytes 3 D.unsignedInt8) 
-       |> Maybe.map (List.reverse >> (List.map hexStringOfInt) >> String.join "")
-Just "616263" : Maybe String
+{-| intOfHexPair "F7" == Just 247
+intOfHexPair "F7" |> Maybe.map hexStringOfInt
 -}
+intOfHexPair : String -> Maybe Int  
+intOfHexPair str = 
+  let 
+    hi_ = intOfHexDigit (String.left 1 str)
+    hi = Maybe.map (\x -> 16*x) hi_
+    lo = intOfHexDigit (String.right 1 str)
+  in
+  Maybe.map2 (+) hi lo
+    
+
 decodeBytes : Int -> Decoder a -> Decoder (List a)
 decodeBytes len decoder = 
   loop (len, []) (listStep decoder)
